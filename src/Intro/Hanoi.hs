3b36a7379bfb650e4aa646becca8342ebb91bc55
Dan Brooks <dmbrooks@live.co.uk>
2017-05-22 08:48:39 +0100

    Solve hanoi four, sub optimal solution

--- src/Intro/Hanoi.hs
+++ src/Intro/Hanoi.hs
@@ -8,3 +8,9 @@ hanoi 0 _ _ _ = []
 hanoi 1 a b _ = [(a, b)]
 hanoi n a b c = (hanoi (n - 1) a c b) ++ (hanoi 1 a b c) ++ (hanoi (n - 1) c b a)
 
+hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 0 _ _ _ _ = []
+hanoi4 1 a b _ _ = [(a,b)]
+hanoi4 2 a b _ d = (hanoi 2 a b d)
+hanoi4 3 a b c d = (hanoi4 1 a d c b) ++ (hanoi4 2 a b d c) ++ (hanoi4 1 d b c a)
+hanoi4 n a b c d = (hanoi4 (n - 3) a d c b) ++ (hanoi 3 a b c) ++ (hanoi4 (n - 3) d b c a)
--- test/Tests/Intro/Hanoi.hs
+++ test/Tests/Intro/Hanoi.hs
@@ -9,12 +9,28 @@ isEqual = assertEqual ""
 
 hanoiTest :: TestTree
 hanoiTest = testGroup "hanoi"
-  [ testCase "one disc" $ isEqual [("a", "b")] (hanoi 1 "a" "b" "c")
-  , testCase "two discs" $ isEqual [("a", "c"), ("a", "b"), ("c", "b")] (hanoi 2 "a" "b" "c")
-  , testCase "three discs" $ isEqual [("a", "b"), ("a", "c"), ("b", "c"), ("a", "b"), ("c", "a"), ("c", "b"), ("a", "b")] (hanoi 3 "a" "b" "c")
+  [ testCase "one disc" $ isEqual [("a","b")] (hanoi 1 "a" "b" "c")
+  , testCase "two discs" $ isEqual [("a","c"),("a","b"),("c","b")] (hanoi 2 "a" "b" "c")
+  , testCase "three discs" $ isEqual [("a","b"),("a","c"),("b","c"),("a","b"),("c","a"),("c","b"),("a","b")] (hanoi 3 "a" "b" "c")
+  ]
+
+hanoi4Test :: TestTree
+hanoi4Test = testGroup "hanoi 4 pegs"
+  [ testCase "one disc"
+    $ isEqual [("a","b")] (hanoi4 1 "a" "b" "c" "d")
+  , testCase "two discs"
+    $ isEqual [("a","d"),("a","b"),("d","b")] (hanoi4 2 "a" "b" "c" "d")
+  , testCase "three discs"
+    $ isEqual [("a","d"),("a","c"),("a","b"),("c","b"),("d","b")] (hanoi4 3 "a" "b" "c" "d")
+  , testCase "four discs"
+    $ isEqual [("a","d"),("a","b"),("a","c"),("b","c"),("a","b"),("c","a"),("c","b"),("a","b"),("d","b")] (hanoi4 4 "a" "b" "c" "d")
+  , testCase "takes a lot fewer moves to complete"
+    -- $ isEqual 129 (length $ hanoi4 15 "a" "b" "c" "d")
+    $ isEqual 185 (length $ hanoi4 15 "a" "b" "c" "d")
   ]
 
 tests :: TestTree
 tests = testGroup "intro"
   [ hanoiTest
+  , hanoi4Test
   ]
